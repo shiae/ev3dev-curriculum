28ea0bed8fa019e5e4526fb72f9432ab9357e248
Allison Shi <shiae@rose-hulman.edu>
2018-01-25 16:37:54 -0500

    Maybe this works?

--- sandbox/shiae/motors/m1_drive_timed.py
+++ sandbox/shiae/motors/m1_drive_timed.py
@@ -92,10 +92,11 @@ Authors: David Fisher and Allison Shi.
 
 
 def main():
-    print("--------------------------------------------")
-    print("  Drive using input")
-    print("--------------------------------------------")
-    ev3.Sound.speak("Drive using input").wait()
+
+       print("--------------------------------------------")
+       print("  Timed Driving")
+       print("--------------------------------------------")
+    ev3.Sound.speak("Timed Driving").wait()
 
     # Connect two large motors on output ports B and C
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
@@ -107,11 +108,13 @@ def main():
 
     time_s = 1  # Any value other than 0.
     while time_s != 0:
-        left_sp = int(input("Enter a speed for the left motor (0 to 900 dps): "))
-        right_sp = int(input("Enter a speed for the right motor (0 to 900 dps): "))
-        time_s = int(input("Enter a time to drive (seconds): "))
-        left_motor.run_forever(speed_sp=left_sp)
-        right_motor.run_forever(speed_sp=right_sp)
+        v = int(input("Enter a speed (0 to 900 dps): "))
+        x = int(input("Distance to travel (inches): "))
+
+        time_s = (360/v)*(x/4.3)
+
+        left_motor.run_forever(speed_sp=v)
+        right_motor.run_forever(speed_sp=v)
         time.sleep(time_s)
         left_motor.stop()
         right_motor.stop(stop_action="brake")
