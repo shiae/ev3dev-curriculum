bfa45c2d97c65a56568dae78fc243fabf17e1f2f
Allison Shi <shiae@rose-hulman.edu>
2018-02-05 16:51:00 -0500

    m3 more done than before

--- sandbox/shiae/mqtt/m3_ev3_led_button_communication.py
+++ sandbox/shiae/mqtt/m3_ev3_led_button_communication.py
@@ -32,8 +32,8 @@ communication.  Summary of the communication:
 Implement the TODOs below to complete this module, then transfer the file to the EV3 (as done in many previous units),
   then run this module on the EV3 while at the same time, running m3_pc_led_button_communication.py on your PC.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Ryan, Stephen, Allison.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 import mqtt_remote_method_calls as com
@@ -71,6 +71,28 @@ class MyDelegate(object):
 
     def __init__(self):
         self.running = True
+
+    def set_led(self, led_side_string, led_color_string):
+        print("Received: {} {}".format(led_side_string, led_color_string))
+        led_side = None
+        if led_side_string == "left":
+            led_side = ev3.Leds.LEFT
+        elif led_side_string == "right":
+            led_side = ev3.Leds.RIGHT
+
+        led_color = None
+        if led_color_string == "green":
+            led_color = ev3.Leds.GREEN
+        elif led_color_string == "red":
+            led_color = ev3.Leds.RED
+        elif led_color_string == "black":
+            led_color = ev3.Leds.BLACK
+
+        if led_side is None or led_color is None:
+            print("Invalid parameters sent to set_led. led_side_string = {} led_color_string = {}".format(
+                led_side_string, led_color_string))
+        else:
+            ev3.Leds.set_color(led_side, led_color)
 
 
 def main():
