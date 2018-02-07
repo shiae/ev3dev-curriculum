3ea301147bf0a649da848df5bdfd7eea1bfe9261
Allison Shi <shiae@rose-hulman.edu>
2018-02-07 16:54:25 -0500

    m1 more done than before

--- sandbox/shiae/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/shiae/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -58,11 +58,12 @@ def main():
 
     # For our standard shutdown button.
     btn = ev3.Button()
-    # TODO: 2. Uncomment the lines below to setup event handlers for these buttons.
-    # btn.on_up = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_RED)
-    # btn.on_down = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLUE)
-    # btn.on_left = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLACK)
-    # btn.on_right = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_WHITE)
+    # DONE: 2. Uncomment the lines below to setup event handlers for these
+    # buttons.
+    btn.on_up = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_RED)
+    btn.on_down = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLUE)
+    btn.on_left = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLACK)
+    btn.on_right = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_WHITE)
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
 
     while dc.running:
@@ -86,6 +87,10 @@ def drive_to_color(button_state, robot, color_to_seek):
       :type robot: robo.Snatch3r
       :type color_to_seek: int
     """
+
+    robot.color_sensor = ev3.ColorSensor()
+    assert robot.color_sensor
+
     if button_state:
         ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
 
@@ -95,6 +100,7 @@ def drive_to_color(button_state, robot, color_to_seek):
         #   assert self.color_sensor
         # Then here you can use a command like robot.color_sensor.color to check the value
 
+        robot.color_sensor.color
 
 
         # TODO: 4. Call over a TA or instructor to sign your team's checkoff sheet.
