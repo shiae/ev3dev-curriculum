20a72c0611f7a36da894ae8425f9f5afd056e956
fleethrd <fleethrd@rose-hulman.edu>
2018-02-12 21:07:18 -0500

    wip

--- projects/fleethrd/pc_final.py
+++ projects/fleethrd/pc_final.py
@@ -1,16 +1,92 @@
 
-
+import tkinter
+from tkinter import ttk
 
 
 def main():
+    root = tkinter.Tk()
+    gui(root)
+    root.mainloop()
+    # settings = ['a', 'a', 'a']
+    # data = ['a', 'a', 'a']
+    #  enigma(settings, data)
+    # print(data)
+
+
+def gui(root):
+    root.title("Enigma")
+
+    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame.grid()
+
+    first_input_label = ttk.Label(main_frame, text="First Input")
+    first_input_label.grid(row=0, column=0)
+    first_input = ttk.Entry(main_frame, width=8)
+    first_input.insert(0, "a")
+    first_input.grid(row=1, column=0)
+
+    second_input_label = ttk.Label(main_frame, text="Second Input")
+    second_input_label.grid(row=0, column=2)
+    second_input = ttk.Entry(main_frame, width=8, justify=tkinter.CENTER)
+    second_input.insert(0, "a")
+    second_input.grid(row=1, column=2)
+
+    third_input_label = ttk.Label(main_frame, text="Third Input")
+    third_input_label.grid(row=0, column=4)
+    third_input = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    third_input.insert(0, "a")
+    third_input.grid(row=1, column=4)
+
+    first_settings_input_label = ttk.Label(main_frame, text="First Setting")
+    first_settings_input_label.grid(row=3, column=0)
+    first_settings_input = ttk.Entry(main_frame, width=8)
+    first_settings_input.insert(0, "a")
+    first_settings_input.grid(row=4, column=0)
+
+    second_settings_input_label = ttk.Label(main_frame, text="Second Setting")
+    second_settings_input_label.grid(row=3, column=2)
+    second_settings_input = ttk.Entry(main_frame, width=8,
+                                      justify=tkinter.CENTER)
+    second_settings_input.insert(0, "a")
+    second_settings_input.grid(row=4, column=2)
+
+    third_settings_input_label = ttk.Label(main_frame, text="Third Setting")
+    third_settings_input_label.grid(row=3, column=4)
+    third_settings_input = ttk.Entry(main_frame, width=8,
+                                     justify=tkinter.RIGHT)
+    third_settings_input.insert(0, "a")
+    third_settings_input.grid(row=4, column=4)
+
+    encipher_button = ttk.Button(main_frame, text="Encipher")
+    encipher_button.grid(row=6, column=2)
+    encipher_button['command'] = (lambda: enigma(first_input, second_input,
+                                                 third_input,
+                                                 first_settings_input,
+                                                 second_settings_input,
+                                                 third_settings_input))
+
+
+def enigma(first_input, second_input, third_input, first_settings_input,
+           second_settings_input, third_settings_input):
     x = [2, 0, 1, 3]
     y = [3, 2, 1, 0]
     z = [1, 3, 2, 0]
     r = [2, 3, 0, 1]
     wheels = [0, x, y, z, r]
-    data = ['a', 'a', 'a']
+    settings = [first_settings_input.get(), second_settings_input.get(),
+                third_settings_input.get()]
+    data = [first_input.get(), second_input.get(), third_input.get()]
+    set_up(settings, wheels)
     encryption(data, wheels)
     print(data)
+
+
+def set_up(settings, wheels):
+    letters_to_numbers(settings)
+    for k in range(len(settings)):
+        for j in range(settings[k]):
+            rotation(wheels[k+1])
+    numbers_to_letters(settings)
 
 
 def encryption(data, wheels):
