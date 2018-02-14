e7aa3e84e247219e19ab73fd56835606f6c50707
Allison Shi <shiae@rose-hulman.edu>
2018-02-14 15:29:49 -0500

    More done than before

--- projects/shiae/final_project_ev3.py
+++ projects/shiae/final_project_ev3.py
@@ -2,3 +2,20 @@
 
 
 """
+
+import ev3dev.ev3 as ev3
+import time
+
+import tkinter
+from tkinter import ttk
+
+import mqtt_remote_method_calls as com
+import robot_controller as robo
+
+
+def main():
+    robit = robo.Snatch3r()
+    mqtt_client = com.MqttClient(robit)
+    mqtt_client.connect_to_pc()
+    robit.loop_forever()  # Calls a function that has a while True: loop within
+    # it to avoid letting the program end.
--- projects/shiae/final_project_pc.py
+++ projects/shiae/final_project_pc.py
@@ -2,18 +2,18 @@
 # functions: 5-20 lines of code
 #
 
-import ev3dev.ev3 as ev3
-import time
-
 import tkinter
 from tkinter import ttk
 
 import mqtt_remote_method_calls as com
-import robot_controller as robo
 
-robit = robo.Snatch3r()
 
-robit.loop_forever()  # Calls a function that has a while True: loop within
-# it to avoid letting the program end.
+def main():
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+
+    root = tkinter.Tk()
+    root.title("Robit")
+
 
 
