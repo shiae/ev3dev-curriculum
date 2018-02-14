7d938bb55c00bc12cc88743d126933c6f92a4e2f
Allison Shi <shiae@rose-hulman.edu>
2018-02-14 15:36:11 -0500

    color tracking

--- projects/shiae/final_project_ev3.py
+++ projects/shiae/final_project_ev3.py
@@ -3,19 +3,51 @@
 
 """
 
-import ev3dev.ev3 as ev3
+# import ev3dev.ev3 as ev3
 import time
-
-import tkinter
-from tkinter import ttk
-
-import mqtt_remote_method_calls as com
+# import tkinter
+# from tkinter import ttk
 import robot_controller as robo
 
 
 def main():
     robit = robo.Snatch3r()
-    mqtt_client = com.MqttClient(robit)
-    mqtt_client.connect_to_pc()
-    robit.loop_forever()  # Calls a function that has a while True: loop within
-    # it to avoid letting the program end.
+    # mqtt_client = com.MqttClient(robit)
+    # mqtt_client.connect_to_pc()
+    # robit.loop_forever()  # Calls a function that has a while True: loop within
+    # # it to avoid letting the program end.
+
+    print("--------------------------------------------")
+    print(" Color tracking")
+    print("--------------------------------------------")
+    print("Press the touch sensor to exit this program.")
+
+    robit.pixy.mode = "SIG1"
+    turn_speed = 100
+
+    while not robit.touch_sensor.is_pressed:
+
+        x = robit.pixy.value(1)
+        y = robit.pixy.value(2)
+
+        print("(X, Y)=({}, {})".format(x, y))
+
+        if x < 150:
+            robit.turn(turn_speed, turn_speed)
+            while robit.pixy.value(1) < 150 and \
+                            robit.touch_sensor.is_pressed is False:
+                time.sleep(.01)
+        elif x > 170:
+            robit.turn(-turn_speed, -turn_speed)
+            while robit.pixy.value(1) > 170 and \
+                            robit.touch_sensor.is_pressed is False:
+                time.sleep(.01)
+        else:
+            robit.stop()
+
+        time.sleep(0.25)
+
+    robit.shutdown()
+
+
+main()
--- projects/shiae/final_project_pc.py
+++ projects/shiae/final_project_pc.py
@@ -4,6 +4,8 @@
 
 import tkinter
 from tkinter import ttk
+import time
+
 
 import mqtt_remote_method_calls as com
 
@@ -12,8 +14,24 @@ def main():
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
 
-    root = tkinter.Tk()
-    root.title("Robit")
+    # root = tkinter.Tk()
+    # root.title("Robit!")
+    #
+    # main_frame = ttk.Frame(root, padding=20, relief='raised')
+    # main_frame.grid()
+    #
+    # left_speed_label = ttk.Label(main_frame, text="Left")
+    # left_speed_label.grid(row=0, column=0)
+    # left_speed_entry = ttk.Entry(main_frame, width=8)
+    # left_speed_entry.insert(0, "600")
+    # left_speed_entry.grid(row=1, column=0)
+    #
+    # right_speed_label = ttk.Label(main_frame, text="Right")
+    # right_speed_label.grid(row=0, column=2)
+    # right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    # right_speed_entry.insert(0, "600")
+    # right_speed_entry.grid(row=1, column=2)
+
 
 
 
