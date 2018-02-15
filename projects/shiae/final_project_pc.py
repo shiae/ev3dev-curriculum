2860bf197dbfb675cf9599301559866bb8794046
Allison Shi <shiae@rose-hulman.edu>
2018-02-15 17:43:45 -0500

    button works

--- projects/shiae/final_project_pc.py
+++ projects/shiae/final_project_pc.py
@@ -8,25 +8,21 @@ from tkinter import ttk
 # import mqtt_remote_method_calls as com
 
 
-# class MyDelegate(object):
-#     #   Creates an object which can write a message to a shared chat board.
-#     def __init__(self, label):
-#         self.label = label
-
-    # def on_chat_message(self, message):
-    #     self.label["text"] += "\n" + message
-
-
 def main():
-    command()
-
-
-def command():
+    # command()
     root = tkinter.Tk()
-    frame1 = ttk.Frame(root, padding=20)
+
+    frame1 = ttk.Frame(root, padding=30)
     frame1.grid()
 
+    hello_button = ttk.Button(frame1, text="Hello!")
+    hello_button['command'] = (lambda: print("Hello!"))
+    hello_button.grid()
+
     root.mainloop()
+
+# def command():
+
 
 
 main()
