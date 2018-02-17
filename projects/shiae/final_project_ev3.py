b74b962d32dc30061fac38a6f860391b8a9dd483
Allison Shi <shiae@rose-hulman.edu>
2018-02-17 17:10:38 -0500

    robit better than before

--- projects/shiae/final_project_ev3.py
+++ projects/shiae/final_project_ev3.py
@@ -21,16 +21,78 @@ class MyDelegate(object):
         print("--------------------------------------------")
         ev3.Sound.speak('bark bark')
 
+    def sit(self):
+        print("--------------------------------------------")
+        print(" Sit ev3")
+        print("--------------------------------------------")
+        robit.stop()
+
+    def shake(self):
+        print("--------------------------------------------")
+        print(" Shake")
+        print("--------------------------------------------")
+        ev3.Sound.speak("shake")
+        while not robit.touch_sensor.is_pressed:
+            if robit.ir_sensor.proximity < 7:
+                robit.shake()
+        print("Exit Shake")
+
+    def fetch(self):
+        print("--------------------------------------------")
+        print(" Fetch ev3")
+        print("--------------------------------------------")
+        print("Press the touch sensor to exit this program.")
+        # current_distance = robit.beacon_seeker.distance
+        robit.pixy.mode = "SIG1"
+        turn_speed = 100
+        forward_speed = 300
+        robit.pixy.mode = "SIG1"
+        while not robit.touch_sensor.is_pressed:
+
+            x = robit.pixy.value(1)
+            y = robit.pixy.value(2)
+            width = robit.pixy.value(3)
+            height = robit.pixy.value(4)
+            area = width * height
+            close_enough = \
+                7489213479018234709812374092183740921834709128347012938471239084712903847123908471239847
+
+            print("(X, Y)=({}, {})".format(x, y))
+
+            if x < 150:
+                robit.turn(turn_speed, turn_speed)
+                while robit.pixy.value(
+                        1) < 150 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            elif x > 170:
+                robit.turn(-turn_speed, -turn_speed)
+                while robit.pixy.value(
+                        1) > 170 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            else:
+                robit.stop()
+                if area > close_enough:
+                    robit.drive(forward_speed, forward_speed)
+                    time.sleep(0.01)
+                else:
+                    robit.stop()
+                    ev3.Sound.speak("Woof!")
+                    time.sleep(2)
+
+            time.sleep(0.25)
+
+        robit.shutdown()
+
 
 def main():
-    # print("--------------------------------------------")
-    # print(" Calibrating")
-    # print("--------------------------------------------")
     my_delegate = MyDelegate()
     mqtt_client = com.MqttClient(my_delegate)
     mqtt_client.connect_to_pc()
     robit.loop_forever()
-    # robit.arm_calibration()
+    print("--------------------------------------------")
+    print(" Calibrating")
+    print("--------------------------------------------")
+    robit.arm_calibration()
     # friend_tracking()
     # shake()
     # mqtt_client = com.MqttClient(robit)
@@ -38,63 +100,63 @@ def main():
     # robit.loop_forever()  # Calls a function that has a while True: loop within
     # # it to avoid letting the program end.
 
+#
+# def friend_tracking():
+#     print("--------------------------------------------")
+#     print(" Friend tracking")
+#     print("--------------------------------------------")
+#     print("Press the touch sensor to exit this program.")
+#     # current_distance = robit.beacon_seeker.distance
+#     robit.pixy.mode = "SIG1"
+#     turn_speed = 100
+#     # forward_speed = 300
+#     robit.pixy.mode = "SIG1"
+#     while not robit.touch_sensor.is_pressed:
+#
+#         x = robit.pixy.value(1)
+#         y = robit.pixy.value(2)
+#         # width = robit.pixy.value(3)
+#         # height = robit.pixy.value(4)
+#         # area = width * height
+#
+#         print("(X, Y)=({}, {})".format(x, y))
+#
+#         if x < 150:
+#             robit.turn(turn_speed, turn_speed)
+#             while robit.pixy.value(1) < 150 and robit.touch_sensor.is_pressed is False:
+#                 time.sleep(.01)
+#         elif x > 170:
+#             robit.turn(-turn_speed, -turn_speed)
+#             while robit.pixy.value(1) > 170 and robit.touch_sensor.is_pressed is False:
+#                 time.sleep(.01)
+#         else:
+#             robit.stop()
+#             time.sleep(2)
+#             # if current_distance == -128:
+#             #     print("where are you?")
+#             # elif current_distance > 10:
+#             #     robit.drive(forward_speed, forward_speed)
+#             #     time.sleep(3)
+#             # else:
+#             #     robit.stop()
+#             #     ev3.Sound.speak("Woof!")
+#
+#         time.sleep(0.25)
+#
+#     robit.shutdown()
 
-def friend_tracking():
-    print("--------------------------------------------")
-    print(" Friend tracking")
-    print("--------------------------------------------")
-    print("Press the touch sensor to exit this program.")
-    # current_distance = robit.beacon_seeker.distance
-    robit.pixy.mode = "SIG1"
-    turn_speed = 100
-    # forward_speed = 300
-    robit.pixy.mode = "SIG1"
-    while not robit.touch_sensor.is_pressed:
 
-        x = robit.pixy.value(1)
-        y = robit.pixy.value(2)
-        # width = robit.pixy.value(3)
-        # height = robit.pixy.value(4)
-        # area = width * height
-
-        print("(X, Y)=({}, {})".format(x, y))
-
-        if x < 150:
-            robit.turn(turn_speed, turn_speed)
-            while robit.pixy.value(1) < 150 and robit.touch_sensor.is_pressed is False:
-                time.sleep(.01)
-        elif x > 170:
-            robit.turn(-turn_speed, -turn_speed)
-            while robit.pixy.value(1) > 170 and robit.touch_sensor.is_pressed is False:
-                time.sleep(.01)
-        else:
-            robit.stop()
-            time.sleep(2)
-            # if current_distance == -128:
-            #     print("where are you?")
-            # elif current_distance > 10:
-            #     robit.drive(forward_speed, forward_speed)
-            #     time.sleep(3)
-            # else:
-            #     robit.stop()
-            #     ev3.Sound.speak("Woof!")
-
-        time.sleep(0.25)
-
-    robit.shutdown()
-
-
-def shake():
-    print("--------------------------------------------")
-    print(" Shake")
-    print("--------------------------------------------")
-
-    ev3.Sound.speak("shake")
-
-    while not robit.touch_sensor.is_pressed:
-        if robit.ir_sensor.proximity < 7:
-            robit.shake()
-    print("Exit Shake")
+# def shake():
+#     print("--------------------------------------------")
+#     print(" Shake")
+#     print("--------------------------------------------")
+#
+#     ev3.Sound.speak("shake")
+#
+#     while not robit.touch_sensor.is_pressed:
+#         if robit.ir_sensor.proximity < 7:
+#             robit.shake()
+#     print("Exit Shake")
 
 
 main()
