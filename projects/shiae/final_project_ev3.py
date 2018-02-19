b11205c6c8bfd3a0d8d4106590c3639e6c4e58c6
Allison Shi <shiae@rose-hulman.edu>
2018-02-19 13:22:48 -0500

    more done than before

--- projects/shiae/final_project_ev3.py
+++ projects/shiae/final_project_ev3.py
@@ -4,7 +4,7 @@ make everything methods in MyDelegate!
 """
 
 import ev3dev.ev3 as ev3
-# import time
+import time
 import robot_controller as robo
 import mqtt_remote_method_calls as com
 robit = robo.Snatch3r()
@@ -37,93 +37,93 @@ class MyDelegate(object):
                 robit.shake()
         print("Exit Shake")
 
-    # def fetch(self):
-    #     print("--------------------------------------------")
-    #     print(" Fetch ev3")
-    #     print("--------------------------------------------")
-    #     print("Press the touch sensor to exit this program.")
-    #     robit.pixy.mode = "SIG1"
-    #     turn_speed = 100
-    #     forward_speed = 300
-    #     while not robit.touch_sensor.is_pressed:
-    #
-    #         x = robit.pixy.value(1)
-    #         y = robit.pixy.value(2)
-    #         width = robit.pixy.value(3)
-    #         height = robit.pixy.value(4)
-    #         area = width * height
-    #         close_enough = \
-    #             7489213479018234709812374092183740921834709128347012938471239084712903847123908471239847
-    #
-    #         print("(X, Y)=({}, {})".format(x, y))
-    #
-    #         if x < 150:
-    #             robit.turn(turn_speed, turn_speed)
-    #             while robit.pixy.value(
-    #                     1) < 150 and robit.touch_sensor.is_pressed is False:
-    #                 time.sleep(.01)
-    #         elif x > 170:
-    #             robit.turn(-turn_speed, -turn_speed)
-    #             while robit.pixy.value(
-    #                     1) > 170 and robit.touch_sensor.is_pressed is False:
-    #                 time.sleep(.01)
-    #         else:
-    #             robit.stop()
-    #             if area > close_enough:
-    #                 robit.drive(forward_speed, forward_speed)
-    #                 time.sleep(0.01)
-    #             else:
-    #                 robit.stop()
-    #                 ev3.Sound.speak("Woof!")
-    #                 time.sleep(2)
-    #
-    #         time.sleep(0.25)
-    #
-    #     robit.shutdown()
-    #
-    # def come(self):
-    #     print("--------------------------------------------")
-    #     print(" Come ev3")
-    #     print("--------------------------------------------")
-    #     print("Press the touch sensor to exit this program.")
-    #     robit.pixy.mode = "SIG2"
-    #     turn_speed = 100
-    #     forward_speed = 300
-    #     while not robit.touch_sensor.is_pressed:
-    #
-    #         x = robit.pixy.value(1)
-    #         y = robit.pixy.value(2)
-    #         width = robit.pixy.value(3)
-    #         height = robit.pixy.value(4)
-    #         area = width * height
-    #         close_enough = \
-    #             7489213479018234709812374092183740921834709128347012938471239084712903847123908471239847
-    #
-    #         print("(X, Y)=({}, {})".format(x, y))
-    #
-    #         if x < 150:
-    #             robit.turn(turn_speed, turn_speed)
-    #             while robit.pixy.value(
-    #                     1) < 150 and robit.touch_sensor.is_pressed is False:
-    #                 time.sleep(.01)
-    #         elif x > 170:
-    #             robit.turn(-turn_speed, -turn_speed)
-    #             while robit.pixy.value(
-    #                     1) > 170 and robit.touch_sensor.is_pressed is False:
-    #                 time.sleep(.01)
-    #         else:
-    #             robit.stop()
-    #             if area > close_enough:
-    #                 robit.drive(forward_speed, forward_speed)
-    #                 time.sleep(0.01)
-    #             else:
-    #                 robit.stop()
-    #                 ev3.Sound.speak("Woof!")
-    #                 time.sleep(2)
-    #
-    #         time.sleep(0.25)
-    #
-    #     robit.shutdown()
+    def fetch(self):
+        print("--------------------------------------------")
+        print(" Fetch ev3")
+        print("--------------------------------------------")
+        print("Press the touch sensor to exit this program.")
+        robit.pixy.mode = "SIG1"
+        turn_speed = 100
+        forward_speed = 300
+        while not robit.touch_sensor.is_pressed:
+
+            x = robit.pixy.value(1)
+            y = robit.pixy.value(2)
+            width = robit.pixy.value(3)
+            height = robit.pixy.value(4)
+            area = width * height
+            close_enough = \
+                7489213479018234709812374092183740921834709128347012938471239084712903847123908471239847
+
+            print("(X, Y)=({}, {})".format(x, y))
+
+            if x < 150:
+                robit.turn(turn_speed, turn_speed)
+                while robit.pixy.value(
+                        1) < 150 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            elif x > 150:
+                robit.turn(-turn_speed, -turn_speed)
+                while robit.pixy.value(
+                        1) > 170 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            else:
+                robit.stop()
+                if area > close_enough:
+                    robit.drive(forward_speed, forward_speed)
+                    time.sleep(0.01)
+                else:
+                    robit.stop()
+                    ev3.Sound.speak("Woof!")
+                    time.sleep(2)
+
+            time.sleep(0.25)
+
+        robit.shutdown()
+
+    def come(self):
+        print("--------------------------------------------")
+        print(" Come ev3")
+        print("--------------------------------------------")
+        print("Press the touch sensor to exit this program.")
+        robit.pixy.mode = "SIG2"
+        turn_speed = 100
+        forward_speed = 300
+        while not robit.touch_sensor.is_pressed:
+
+            x = robit.pixy.value(1)
+            y = robit.pixy.value(2)
+            width = robit.pixy.value(3)
+            height = robit.pixy.value(4)
+            area = width * height
+            close_enough = \
+                7489213479018234709812374092183740921834709128347012938471239084712903847123908471239847
+
+            print("(X, Y)=({}, {})".format(x, y))
+
+            if x < 150:
+                robit.turn(turn_speed, turn_speed)
+                while robit.pixy.value(
+                        1) < 150 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            elif x > 170:
+                robit.turn(-turn_speed, -turn_speed)
+                while robit.pixy.value(
+                        1) > 170 and robit.touch_sensor.is_pressed is False:
+                    time.sleep(.01)
+            else:
+                robit.stop()
+                if area > close_enough:
+                    robit.drive(forward_speed, forward_speed)
+                    time.sleep(0.01)
+                else:
+                    robit.stop()
+                    ev3.Sound.speak("Woof!")
+                    time.sleep(2)
+
+            time.sleep(0.25)
+
+        robit.shutdown()
 
 
 def main():
